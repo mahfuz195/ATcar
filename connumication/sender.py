675ad9b7387fd0f33de6c7634095626a90d41b25
mahfuz195 <mahfuj.islam28@gmail.com>
2016-11-10 13:43:56 -0500

    multicast tx rx added

--- connumication/sender.py
+++ connumication/sender.py
@@ -1,11 +1,15 @@
 import socket
 import sys
+
 def main():
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        my_socket.bind(('',0))
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+
+        dest = ('<broadcast>',8881)
         while 1:
                 data = raw_input('Enter data:')
-                my_socket.sendto(data, ('255.255.255.255' ,8881))
+                my_socket.sendto(data, dest)
 
         my_socket.close()
 	
